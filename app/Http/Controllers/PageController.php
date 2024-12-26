67e1ea40cd27de04ff242f5789f852ef0c1fb027
Jose insfran <insfranjosealfredo@gmail.com>
2024-12-26 18:37:42 -0300

    Crear pagina de error y Ok..- Poner nombre a los campos

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,14 +9,17 @@ class PageController extends Controller
 {
     public function index(Request $request)
     {
-        /*$validated = $request->validate([
-            'referred' => 'required|string|max:255',
-        ]);*/
-
-        $referredCode = $request->input("referred");
-        $agente = User::firstWhere("referral_code",$referredCode);
-
-        //return view('pages.form',compact('agente'));
-        return view('pages.formmbg',compact('agente'));
+        if($request->has('error')){
+            $errorMsg = $request->error;
+            return view('pages.result',compact('errorMsg'));
+        }elseif($request->has('referred')){
+            $referredCode = $request->input("referred");
+            $agente = User::firstWhere("referral_code",$referredCode);
+            
+            return view('pages.formmbg',compact('agente'));
+        }elseif($request->has('success')){
+            $okMsg = "Form saved successfully ";
+            return view('pages.result',compact('okMsg'));
+        }
     }
 }
--- resources/views/pages/formmbg.blade.php
+++ resources/views/pages/formmbg.blade.php
@@ -38,69 +38,84 @@
         </div>
 
         <!-- Información del agente -->
-        <div class="text-center mb-4">
-            <h2>Información del Agente</h2>
-            <p class="text-muted">Nombre: <strong>Juan Pérez</strong></p>
-            <p class="text-muted">Teléfono: <strong>+595 123 456 789</strong></p>
+        <div class="row">
+            <!-- Información del agente -->
+            <div class="col-md-8">
+                <div class="text-center mb-4">
+                    <h2>Agent Information</h2>
+                    <p class="text-muted">Name: <strong>{{ $agente->name ?? '' }}</strong></p>
+                    <p class="text-muted">NPN Agent: <strong>{{ $agente->agent_npn ?? '' }}</strong></p>
+                </div>
+            </div>
+             <!-- Espacio para la foto -->
+             <div class="col-md-4 text-center mb-4">
+                <img src="{{ $agente->profile_photo ?? 'placeholder.jpg' }}" alt="Foto del Agente" class="img-fluid rounded-circle"  style="width: 35%;">
+            </div>
         </div>
         <hr>
-        <h2 class="text-center mb-4">Formulario de Seguro</h2>
-        <form>
+        <form id="__vtigerWebForm" name="Occidental Life Form" action="https://crm.callmbg.com/modules/Webforms/capture.php" method="post" accept-charset="utf-8" enctype="multipart/form-data">
             <!-- Datos del Asegurado -->
             <div class="container">
-                <h4 class="mb-3">Datos del Asegurado</h4>
+                <!-- Hidden Fields PARA VTIGER NO TOCAR -->
+                <input type="hidden" name="__vtrftk" value="sid:161cdebb6efa746937e63773fca9ccfc012f46c7,1735243994">
+                <input type="hidden" name="publicid" value="1cd093b59e60d84c4e200e4645ea98f9">
+                <input type="hidden" name="urlencodeenable" value="1">
+                <input type="hidden" name="name" value="Occidental Life Form">
+                <!-- Hidden Fields -->
+
+                <h4 class="mb-3">Insured Information</h4>
                 <div class="row g-3">
                     <!-- Nombre y Apellido -->
                     <div class="col-md-6">
                         <label class="form-label" for="firstName">First Name</label>
-                        <input class="form-control" id="firstName" type="text" placeholder="First Name" data-sb-validations="required" />
+                        <input class="form-control" id="firstname" name="firstname" type="text" placeholder="First Name" data-sb-validations="required" />
                         <div class="invalid-feedback" data-sb-feedback="firstName:required">First Name is required.</div>
                     </div>
                     <div class="col-md-6">
                         <label class="form-label" for="lastName">Last Name</label>
-                        <input class="form-control" id="lastName" type="text" placeholder="Last Name" data-sb-validations="required" />
+                        <input class="form-control" id="lastName" name="lastname" type="text" placeholder="Last Name" data-sb-validations="required" />
                         <div class="invalid-feedback" data-sb-feedback="lastName:required">Last Name is required.</div>
                     </div>
             
                     <!-- Fechas y Edad -->
                     <div class="col-md-4">
                         <label class="form-label" for="dobMmDdYyyy">DOB (mm/dd/yyyy)</label>
-                        <input class="form-control" id="dobMmDdYyyy" type="date" placeholder="DOB (mm/dd/yyyy)" data-sb-validations="required" pattern="\d{2}/\d{2}/\d{4}" />
+                        <input class="form-control" id="dobMmDdYyyy" name="cf_853" type="date" placeholder="DOB (mm/dd/yyyy)" data-sb-validations="required"/>
                         <div class="invalid-feedback" data-sb-feedback="dobMmDdYyyy:required">DOB (mm/dd/yyyy) is required.</div>
                     </div>
                     <div class="col-md-4">
-                        <label class="form-label" for="appDateMmDdYyyy">App date (mm/dd/yyyy)</label>
-                        <input class="form-control" id="appDateMmDdYyyy" type="date" placeholder="App date (mm/dd/yyyy)" data-sb-validations="required" pattern="\d{2}/\d{2}/\d{4}" />
-                        <div class="invalid-feedback" data-sb-feedback="appDateMmDdYyyy:required">App date (mm/dd/yyyy) is required.</div>
+                        <label class="form-label" for="appDate">App date (mm/dd/yyyy)</label>
+                        <input class="form-control" id="appDate" name="cf_949" type="date" placeholder="App date (mm/dd/yyyy)" data-sb-validations="required" pattern="\d{2}/\d{2}/\d{4}" value="{{ now()->format('Y-m-d') }}" />
+                        <div class="invalid-feedback" data-sb-feedback="appDate:required">App date (mm/dd/yyyy) is required.</div>
                     </div>
                     <div class="col-md-4">
                         <label class="form-label" for="age">Age</label>
-                        <input class="form-control" id="age" type="number" placeholder="Age" data-sb-validations="required" />
+                        <input class="form-control" id="age" type="number" placeholder="Age" data-sb-validations="required" name="cf_905"/>
                         <div class="invalid-feedback" data-sb-feedback="age:required">Age is required.</div>
                     </div>
             
                     <!-- SSN y Occupation -->
                     <div class="col-md-6">
                         <label class="form-label" for="ssn">SSN</label>
-                        <input class="form-control" id="ssn" type="text" placeholder="SSN" data-sb-validations="required" />
+                        <input class="form-control" id="ssn" type="text" placeholder="SSN" data-sb-validations="required" name="cf_945"/>
                         <div class="invalid-feedback" data-sb-feedback="ssn:required">SSN is required.</div>
                     </div>
                     <div class="col-md-6">
                         <label class="form-label" for="occupation">Occupation</label>
-                        <input class="form-control" id="occupation" type="text" placeholder="Occupation" data-sb-validations="required" />
+                        <input class="form-control" id="occupation" type="text" placeholder="Occupation" data-sb-validations="required" name="cf_947"/>
                         <div class="invalid-feedback" data-sb-feedback="occupation:required">Occupation is required.</div>
                     </div>
             
                     <!-- Contacto -->
                     <div class="col-md-6">
                         <label class="form-label" for="emailAddress">Email Address</label>
-                        <input class="form-control" id="emailAddress" type="email" placeholder="Email Address" data-sb-validations="required,email" />
+                        <input class="form-control" id="emailAddress" type="email" placeholder="Email Address" data-sb-validations="required,email" name="email" />
                         <div class="invalid-feedback" data-sb-feedback="emailAddress:required">Email Address is required.</div>
                         <div class="invalid-feedback" data-sb-feedback="emailAddress:email">Email Address Email is not valid.</div>
                     </div>
                     <div class="col-md-6">
                         <label class="form-label" for="phone">Phone</label>
-                        <input class="form-control" id="phone" type="text" placeholder="Phone" data-sb-validations="required" />
+                        <input class="form-control" id="phone" type="text" placeholder="Phone" data-sb-validations="required" name="mobile" />
                         <div class="invalid-feedback" data-sb-feedback="phone:required">Phone is required.</div>
                     </div>
                 </div>
@@ -112,28 +127,28 @@
                 <!-- Street Address - Ocupa toda la fila -->
                 <div class="col-12">
                     <label class="form-label" for="streetAddress">Street Address</label>
-                    <input class="form-control" id="streetAddress" type="text" placeholder="Street Address" data-sb-validations="required" />
+                    <input class="form-control" id="streetAddress" type="text" placeholder="Street Address" data-sb-validations="required" name="lane" />
                     <div class="invalid-feedback" data-sb-feedback="streetAddress:required">Street Address is required.</div>
                 </div>
                 
                 <!-- City - Ocupa 6 columnas -->
                 <div class="col-md-6">
                     <label class="form-label" for="city">City</label>
-                    <input class="form-control" id="city" type="text" placeholder="City" data-sb-validations="required" />
+                    <input class="form-control" id="city" type="text" placeholder="City" data-sb-validations="required" name="city"/>
                     <div class="invalid-feedback" data-sb-feedback="city:required">City is required.</div>
                 </div>
                 
                 <!-- State - Ocupa 4 columnas -->
                 <div class="col-md-4">
                     <label class="form-label" for="state">State</label>
-                    <input class="form-control" id="state" type="text" placeholder="State" data-sb-validations="required" />
+                    <input class="form-control" id="state" type="text" placeholder="State" data-sb-validations="required" name="state"/>
                     <div class="invalid-feedback" data-sb-feedback="state:required">State is required.</div>
                 </div>
                 
                 <!-- Zip Code - Ocupa 2 columnas -->
                 <div class="col-md-2">
                     <label class="form-label" for="zipCode">Zip Code</label>
-                    <input class="form-control" id="zipCode" type="text" placeholder="Zip Code" data-sb-validations="required" />
+                    <input class="form-control" id="zipCode" type="text" placeholder="Zip Code" data-sb-validations="required" name="code" />
                     <div class="invalid-feedback" data-sb-feedback="zipCode:required">Zip Code is required.</div>
                 </div>
             </div>
@@ -144,15 +159,15 @@
                 <!-- Beneficiario Principal -->
                 <div class="col-md-6">
                     <label class="form-label" for="nameSPrimaryBeneficiary">Name(s) Primary Beneficiary</label>
-                    <input class="form-control" id="nameSPrimaryBeneficiary" type="text" placeholder="Name(s) Primary Beneficiary" data-sb-validations="" />
+                    <input class="form-control" id="nameSPrimaryBeneficiary" type="text" placeholder="Name(s) Primary Beneficiary" data-sb-validations="" name="cf_955" />
                 </div>
                 <div class="col-md-3">
                     <label class="form-label" for="beneficiaryExtra">-</label>
-                    <input class="form-control" id="beneficiaryExtra" type="text" placeholder="-" data-sb-validations="" />
+                    <input class="form-control" id="beneficiaryExtra" type="text" placeholder="-" data-sb-validations="" name="cf_953"/>
                 </div>
                 <div class="col-md-3">
                     <label class="form-label" for="relationship">Relationship</label>
-                    <input class="form-control" id="relationship" type="text" placeholder="Relationship" data-sb-validations="" />
+                    <input class="form-control" id="relationship" type="text" placeholder="Relationship" data-sb-validations="" name="cf_957"/>
                 </div>
             </div>
             <hr>
@@ -161,28 +176,28 @@
                 <!-- Street Address - Ocupa toda la fila -->
                 <div class="col-12">
                     <label class="form-label" for="streetAddress">Street Address</label>
-                    <input class="form-control" id="streetAddress" type="text" placeholder="Street Address" data-sb-validations="required" />
+                    <input class="form-control" id="streetAddress" type="text" placeholder="Street Address" data-sb-validations="required" name="cf_965"/>
                     <div class="invalid-feedback" data-sb-feedback="streetAddress:required">Street Address is required.</div>
                 </div>
                 
                 <!-- City - Ocupa 6 columnas -->
                 <div class="col-md-6">
                     <label class="form-label" for="city">City</label>
-                    <input class="form-control" id="city" type="text" placeholder="City" data-sb-validations="required" />
+                    <input class="form-control" id="city" type="text" placeholder="City" data-sb-validations="required" name="cf_967" />
                     <div class="invalid-feedback" data-sb-feedback="city:required">City is required.</div>
                 </div>
                 
                 <!-- State - Ocupa 4 columnas -->
                 <div class="col-md-4">
                     <label class="form-label" for="state">State</label>
-                    <input class="form-control" id="state" type="text" placeholder="State" data-sb-validations="required" />
+                    <input class="form-control" id="state" type="text" placeholder="State" data-sb-validations="required" name="cf_969" />
                     <div class="invalid-feedback" data-sb-feedback="state:required">State is required.</div>
                 </div>
                 
                 <!-- Zip Code - Ocupa 2 columnas -->
                 <div class="col-md-2">
                     <label class="form-label" for="zipCode">Zip Code</label>
-                    <input class="form-control" id="zipCode" type="text" placeholder="Zip Code" data-sb-validations="required" />
+                    <input class="form-control" id="zipCode" type="text" placeholder="Zip Code" data-sb-validations="required" name="cf_971" />
                     <div class="invalid-feedback" data-sb-feedback="zipCode:required">Zip Code is required.</div>
                 </div>
             </div>
@@ -193,15 +208,15 @@
                 <!-- Beneficiario Principal -->
                 <div class="col-md-6">
                     <label class="form-label" for="nameSContingetBeneficiary">Name(s) Contingent Beneficiary</label>
-                    <input class="form-control" id="nameSContingentBeneficiary" type="text" placeholder="Name(s) Primary Beneficiary" data-sb-validations="" />
+                    <input class="form-control" id="nameSContingentBeneficiary" type="text" placeholder="Name(s) Primary Beneficiary" data-sb-validations="" name="cf_959"/>
                 </div>
                 <div class="col-md-3">
                     <label class="form-label" for="beneficiaryExtra">-</label>
-                    <input class="form-control" id="beneficiaryExtra" type="text" placeholder="-" data-sb-validations="" />
+                    <input class="form-control" id="beneficiaryExtra" type="text" placeholder="-" data-sb-validations="" name="cf_961"/>
                 </div>
                 <div class="col-md-3">
                     <label class="form-label" for="relationship">Relationship</label>
-                    <input class="form-control" id="relationship" type="text" placeholder="Relationship" data-sb-validations="" />
+                    <input class="form-control" id="relationship" type="text" placeholder="Relationship" data-sb-validations="" name="cf_963"/>
                 </div>
             </div>
             <hr>
@@ -210,31 +225,34 @@
                 <!-- Street Address - Ocupa toda la fila -->
                 <div class="col-12">
                     <label class="form-label" for="streetAddress">Street Address</label>
-                    <input class="form-control" id="streetAddress" type="text" placeholder="Street Address" data-sb-validations="required" />
+                    <input class="form-control" id="streetAddress" type="text" placeholder="Street Address" data-sb-validations="required" name="cf_973" />
                     <div class="invalid-feedback" data-sb-feedback="streetAddress:required">Street Address is required.</div>
                 </div>
                 
                 <!-- City - Ocupa 6 columnas -->
                 <div class="col-md-6">
                     <label class="form-label" for="city">City</label>
-                    <input class="form-control" id="city" type="text" placeholder="City" data-sb-validations="required" />
+                    <input class="form-control" id="city" type="text" placeholder="City" data-sb-validations="required" name="cf_975"/>
                     <div class="invalid-feedback" data-sb-feedback="city:required">City is required.</div>
                 </div>
                 
                 <!-- State - Ocupa 4 columnas -->
                 <div class="col-md-4">
                     <label class="form-label" for="state">State</label>
-                    <input class="form-control" id="state" type="text" placeholder="State" data-sb-validations="required" />
+                    <input class="form-control" id="state" type="text" placeholder="State" data-sb-validations="required" name="cf_977"/>
                     <div class="invalid-feedback" data-sb-feedback="state:required">State is required.</div>
                 </div>
                 
                 <!-- Zip Code - Ocupa 2 columnas -->
                 <div class="col-md-2">
                     <label class="form-label" for="zipCode">Zip Code</label>
-                    <input class="form-control" id="zipCode" type="text" placeholder="Zip Code" data-sb-validations="required" />
+                    <input class="form-control" id="zipCode" type="text" placeholder="Zip Code" data-sb-validations="required" name="cf_979"/>
                     <div class="invalid-feedback" data-sb-feedback="zipCode:required">Zip Code is required.</div>
                 </div>
             </div>
+            <!-- Valores ocultos para el formulario NO REMOVER XXX -->
+            <input type="hidden" name="cf_941" data-label="" value="{{$agente->referral_code ?? '' }}">
+            <input type="hidden" name="cf_929" data-label="" value="occ_life_policy">
             <!-- Botón de Enviar -->
             <button type="submit" class="btn btn-primary w-100 mt-3">Enviar Solicitud</button>
         </form>
