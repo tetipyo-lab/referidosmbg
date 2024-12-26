6c591a5e57afab8068dc9fad456338837d61ef85
Jose insfran <insfranjosealfredo@gmail.com>
2024-12-26 19:08:04 -0300

    Arreglar ruta de las fotos

--- resources/views/pages/formmbg.blade.php
+++ resources/views/pages/formmbg.blade.php
@@ -49,7 +49,7 @@
             </div>
              <!-- Espacio para la foto -->
              <div class="col-md-4 text-center mb-4">
-                <img src="{{ $agente->profile_photo ?? 'placeholder.jpg' }}" alt="Foto del Agente" class="img-fluid rounded-circle"  style="width: 35%;">
+                <img src="{{ Storage::url('public/'.$agente->profile_photo) }}" alt="Foto del Agente" class="img-fluid rounded-circle"  style="width: 35%;">
             </div>
         </div>
         <hr>
